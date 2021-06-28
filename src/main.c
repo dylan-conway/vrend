9293700486e6dc4a1769fb4483ef95c2043d58a5
Dylan Conway <dylan.conway567@gmail.com>
2021-06-28 13:18:36 -0700

    hard 60fps limit

--- src/main.c
+++ src/main.c
@@ -7,16 +7,17 @@
 
 SDL_bool running;
 
+double GetDeltaTime(Uint64 start, Uint64 finish);
+
 int main(int argc, char** argv){
 
     running = SDL_TRUE;
 
     INIT_VREND("Vulkan CA", 640, 480);
 
-    // PrintVulkanInstance();
-
     SDL_Event event;
     while(running){
+        Uint64 start_time = SDL_GetPerformanceCounter();
         while(SDL_PollEvent(&event)){
             switch(event.type){
                 case SDL_QUIT:
@@ -31,9 +32,18 @@ int main(int argc, char** argv){
             }
         }
 
-        // DrawFrame();
+        DRAW_VREND();
+
+        Uint64 finish_time = SDL_GetPerformanceCounter();
+        while(GetDeltaTime(start_time, finish_time) < 16.667){
+            finish_time = SDL_GetPerformanceCounter();
+        }
     }
 
     FREE_VREND();
     return 0;
+}
+
+double GetDeltaTime(Uint64 start, Uint64 finish){
+    return (double)(finish - start) / SDL_GetPerformanceFrequency() * 1000;
 }
